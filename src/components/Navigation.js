d53344c3635f7f5a60a60c38b3c39a5355e2e25f
Kidus Tadesse <kidustadesse@Kiduss-MBP.lan>
2024-11-07 21:39:34 -0500

    Nav style created

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,25 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import styled from "styled-components";
-
-const NavContainer = styled.nav`
-  position: fixed;
-  top: 0;
-  width: 100%;
-  padding: 1rem;
-  display: flex;
-  justify-content: space-around;
-  color: white;
-`;
-
-const NavLink = styled(Link)`
-  text-decoration: none;
-  color: white;
-
-  &:hover {
-    color: #61dafb;
-  }
-`;
+import { NavContainer, NavLink } from "../styles/Navigation";
 
 const Navigation = () => {
   return (
